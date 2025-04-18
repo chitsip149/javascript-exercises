190a4cbe4a4a730c1e261c101cf294b65c9027ac
chitsip149 <minhtrangkl03@gmail.com>
2025-04-18 13:38:53 +0800

    Complete the fibonacci function

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+    if (index<0) return "OOPS";
+    if (index==0) return 0;
+    if (index==1) return 1;
+    let a = 0, b=1;
+    let s;
+    for (let i=2; i<=index; i++){
+        s = a+b;
+        a=b;
+        b=s;
+    }
+    return s;
 };
 
 // Do not edit below this line
