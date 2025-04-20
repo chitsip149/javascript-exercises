e999c4cd432b788cf059e5508eb4f3a3ac441fce
chitsip149 <minhtrangkl03@gmail.com>
2025-04-20 12:33:47 +0800

    Adjust the palindromes function to use array functions

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,11 +1,17 @@
 const palindromes = function (string) {
-    string = string.replace(/[.,:!? ]/g, '');
-    string = string.toLowerCase();
-    for (let i = 0; i<string.length/2; i++){
-        if (string[i]!=string[string.length-i-1]) return false;
-    }
-    return true;
+    const chars = "0123456789abcdefghijklmnopqrstuvwxyz";
+
+    const stringCleaned = string
+    .split("")
+    .map(char => char.toLowerCase())
+    .filter(char => chars.includes(char))
+    .join("");
+
+    const stringReversed = stringCleaned.split("").reverse().join("");
+    return stringCleaned === stringReversed;
 };
+
+// palindromes('R ace cAr');
 
 // Do not edit below this line
 module.exports = palindromes;
