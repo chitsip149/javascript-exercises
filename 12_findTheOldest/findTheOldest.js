e5d16bfa05a1d6c43d457e282d9e334caa9b32ce
chitsip149 <minhtrangkl03@gmail.com>
2025-04-20 13:03:53 +0800

    Complete the findTheOldest function
    - Make use of array functions: forEach, reduce

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    people.forEach(person => {
+        if (person.yearOfDeath===undefined){
+            const d = new Date();
+            person.yearOfDeath = d.getFullYear();
+        }
+    });
 
+    return people.reduce((currentOldest, currentPerson) => {
+        if (currentOldest.yearOfDeath - currentOldest.yearOfBirth < currentPerson.yearOfDeath - currentPerson.yearOfBirth ){
+            return currentPerson;
+        }
+        return currentOldest;
+    }, people[0]);
 };
-
 // Do not edit below this line
 module.exports = findTheOldest;
